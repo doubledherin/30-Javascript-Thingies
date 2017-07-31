e89bdb0d2d35a7dc33bc4e88ea2351bfc81d8bc8
Wendy Dherin <wendydherin@gmail.com>
2017-07-30 21:57:04 -0700

    Add rest of html5 canvas.

--- html5-canvas/html5-canvas.js
+++ html5-canvas/html5-canvas.js
@@ -1,8 +1,36 @@
+
 const canvas = document.querySelector("#draw")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const context = canvas.getContext("2d")
-context.strokeStyle = "BADA55"
 context.lineJoin = "round"
 context.lineCap = "round"
+context.lineWidth = 20
+
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+let hue = 0
+
+
+function draw(e) {
+    if (!isDrawing) return
+    context.strokeStyle = `hsl(${hue}, 40%, 50%)`
+    context.beginPath()
+    context.moveTo(lastX, lastY)
+    context.lineTo(e.offsetX, e.offsetY)
+    context.stroke()
+    lastX = e.offsetX
+    lastY = e.offsetY
+    hue = hue < 360 ? hue + 1 : 0
+}
+
+canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true
+    lastX = e.offsetX
+    lastY = e.offsetY
+})
+canvas.addEventListener("mouseup", () => isDrawing = false)
+canvas.addEventListener("mouseout", () => isDrawing = false)
